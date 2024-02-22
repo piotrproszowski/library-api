f4017d01e7d5c2acec914653014eb7d7b5fa0ab5
piotrproszowski <143588034+piotrproszowski@users.noreply.github.com>
2024-02-22 22:39:22 +0100

    Add database module configuration

---
+++ src/database/database.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('database.host'),
+        port: configService.get<number>('database.port'),
+        username: configService.get<string>('database.username'),
+        password: configService.get<string>('database.password'),
+        database: configService.get<string>('database.name'),
+        entities: [],
+        autoLoadEntities: true,
+        synchronize: configService.get('NODE_ENV') === 'development',
+        useUTC: true,
+      }),
+    }),
+  ],
+  exports: [TypeOrmModule],
+})
+export class DatabaseModule {}
