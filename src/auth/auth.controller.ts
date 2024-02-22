ecbb4a012d42eb356921fdd8affaa5a470203e33
piotrproszowski <143588034+piotrproszowski@users.noreply.github.com>
2024-02-22 23:09:55 +0100

    Add login endpoint to AuthController

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { AuthService } from './auth.service';
+import { LocalAuthGuard } from './guards/local-auth.guard';
+import { Public } from './decorators/public.decorator';
+import { LoginDto } from './dto/login.dto';
 
+@ApiTags('auth')
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @UseGuards(LocalAuthGuard)
+  @Public()
+  @Post('login')
+  async login(@Body() data: LoginDto) {
+    return this.authService.login(data);
+  }
+}
