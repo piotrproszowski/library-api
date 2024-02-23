d28f51f0a298df9c348a782f0f5fdd27cb6e1df4
piotrproszowski <143588034+piotrproszowski@users.noreply.github.com>
2024-02-23 18:28:39 +0100

    Add pg dependency and import ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { StructureModule } from './structure/structure.module';
 import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from './config/config.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, StructureModule, DatabaseModule],
+  imports: [
+    ConfigModule,
+    AuthModule,
+    UsersModule,
+    StructureModule,
+    DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
