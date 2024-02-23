533e3c6b45585ee059e30d0c5265f4fe5bf150cf
piotrproszowski <143588034+piotrproszowski@users.noreply.github.com>
2024-02-23 18:45:59 +0100

    Add TypeOrmModule and forwardRef imports in auth and users modules

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { ConfirmationCode } from './entities/confirmation-code.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ConfirmationCode]), UsersModule],
   controllers: [AuthController],
   providers: [AuthService],
+  exports: [TypeOrmModule, AuthService],
 })
 export class AuthModule {}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,18 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { UserLogin } from './entities/user-login.entity';
+import { StructureModule } from 'src/structure/structure.module';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([User, UserLogin]),
+    forwardRef(() => StructureModule),
+  ],
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService],
+  exports: [TypeOrmModule, UsersService],
 })
 export class UsersModule {}
