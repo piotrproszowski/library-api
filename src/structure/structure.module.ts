52e8902fb2fc5567303265a4a891d1f76c359786
piotrproszowski <143588034+piotrproszowski@users.noreply.github.com>
2024-02-23 23:21:53 +0100

    Add imports and providers to StructureModule

--- src/structure/structure.module.ts
+++ src/structure/structure.module.ts
@@ -1,4 +1,14 @@
 import { Module } from '@nestjs/common';
+import { RentRegistryModule } from './rent-registry/rent-registry.module';
+import { BooksModule } from './books/books.module';
+import { RentEventService } from './rent-event/rent-event.service';
+import { RentEventController } from './rent-event/rent-event.controller';
+import { RentEventModule } from './rent-event/rent-event.module';
+import { RentersModule } from './renters/renters.module';
 
-@Module({})
+@Module({
+  imports: [RentRegistryModule, BooksModule, RentEventModule, RentersModule],
+  providers: [RentEventService],
+  controllers: [RentEventController],
+})
 export class StructureModule {}
