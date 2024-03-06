c7519984f98ad177b1b1de8178ce648536533bfd
piotrproszowski <143588034+piotrproszowski@users.noreply.github.com>
2024-03-06 22:53:27 +0100

    Refactor structure module imports and remove unused code

--- src/structure/structure.module.ts
+++ src/structure/structure.module.ts
@@ -8,31 +8,18 @@ import { UsersModule } from 'src/users/users.module';
 import { RentRegistry } from './rent-registry/entity/rent-registry.entity';
 import { Book } from './books/entity/book.entity';
 import { RentEvent } from './rent-event/entity/rent-event.entity';
-import { Renter } from './renters/entity/renter.entity';
 import { RentRegistryController } from './rent-registry/rent-registry.controller';
 import { BooksController } from './books/books.controller';
-import { RentersController } from './renters/renters.controller';
 import { RentRegistryService } from './rent-registry/rent-registry.service';
 import { BooksService } from './books/books.service';
-import { RentersService } from './renters/renters.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([RentRegistry, Book, RentEvent, Renter]),
+    TypeOrmModule.forFeature([RentRegistry, Book, RentEvent]),
     forwardRef(() => UsersModule),
   ],
-  controllers: [
-    RentRegistryController,
-    BooksController,
-    RentEventController,
-    RentersController,
-  ],
-  providers: [
-    RentRegistryService,
-    RentEventService,
-    BooksService,
-    RentersService,
-  ],
+  controllers: [RentRegistryController, BooksController, RentEventController],
+  providers: [RentRegistryService, RentEventService, BooksService],
   exports: [TypeOrmModule],
 })
 export class StructureModule {}
