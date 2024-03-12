904d0a11e8038d6c4e361b87496eb9268c416806
piotrproszowski <143588034+piotrproszowski@users.noreply.github.com>
2024-03-12 23:38:41 +0100

    Add book CRUD operations with error handling

--- src/structure/books/books.service.ts
+++ src/structure/books/books.service.ts
@@ -16,6 +16,12 @@ export class BooksService {
     private booksRepository: Repository<Book>,
   ) {}
 
+  /**
+   * Creates a new book.
+   * @param createBookDto - The data for creating a book.
+   * @returns The created book.
+   * @throws InternalServerErrorException if there is an error creating the book.
+   */
   async create(createBookDto: CreateBookDto) {
     const book = await this.booksRepository.save(createBookDto);
 
@@ -26,6 +32,13 @@ export class BooksService {
     return { book };
   }
 
+  /**
+   * Updates a book by its ID.
+   * @param id - The ID of the book to update.
+   * @param updateBookDto - The data for updating the book.
+   * @returns The updated book.
+   * @throws NotFoundException if the book is not found.
+   */
   async update(id: string, updateBookDto: UpdateBookDto) {
     const book = await this.booksRepository.findOneBy({ id });
 
@@ -38,6 +51,11 @@ export class BooksService {
     return { book: { ...book, ...updateBookDto } };
   }
 
+  /**
+   * Retrieves all books.
+   * @returns All books.
+   * @throws NotFoundException if no books are found.
+   */
   async getAll() {
     const books = await this.booksRepository.find();
 
@@ -48,6 +66,12 @@ export class BooksService {
     return { books };
   }
 
+  /**
+   * Retrieves a book by its ID.
+   * @param id - The ID of the book to retrieve.
+   * @returns The retrieved book.
+   * @throws NotFoundException if the book is not found.
+   */
   async getOne(id: string) {
     const book = await this.booksRepository.findOneBy({ id });
 
@@ -58,6 +82,12 @@ export class BooksService {
     return { book };
   }
 
+  /**
+   * Deletes a book by its ID.
+   * @param id - The ID of the book to delete.
+   * @returns The deleted book.
+   * @throws NotFoundException if the book is not found.
+   */
   async delete(id: string) {
     const book = await this.booksRepository.findOneBy({ id });
 
@@ -70,6 +100,12 @@ export class BooksService {
     return { deletedBook };
   }
 
+  /**
+   * Changes the status of a book by its ID.
+   * @param id - The ID of the book to change the status of.
+   * @param action - The action to perform on the book's status.
+   * @returns The ID of the book and the action performed.
+   */
   async changeStatus(id: string, action: string) {
     // const book = await this.booksRepository.findOneBy({ id });
 
