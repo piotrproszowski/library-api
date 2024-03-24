a712dae461909a54dd0193255b2030a4b5d6685d
piotrproszowski <143588034+piotrproszowski@users.noreply.github.com>
2024-03-24 23:57:47 +0100

    refactor book entity properties

--- src/structure/books/entity/book.entity.ts
+++ src/structure/books/entity/book.entity.ts
@@ -19,17 +19,52 @@ export class Book {
   @Column({ nullable: true })
   description?: string | null;
 
-  @Column({ unique: true, nullable: true })
-  isbn: string;
+  @Column({ nullable: true })
+  publicationLanguage?: string | null;
+
+  @Column({ nullable: true })
+  originalLanguage?: string | null;
+
+  @Column({ nullable: true })
+  numberOfPages?: number | null;
+
+  @Column({ nullable: true })
+  editionNumber?: number | null;
+
+  @Column({ nullable: true })
+  releaseDate?: Date | null;
+
+  @Column({ nullable: true })
+  publicationDate?: Date | null;
+
+  @Column({ nullable: true })
+  format?: string | null;
+
+  @Column({ nullable: true })
+  coverType?: string | null;
 
   @Column({ nullable: true })
   author?: string | null;
 
   @Column({ nullable: true })
-  price?: string | null;
+  publisher?: string | null;
+
+  @Column({ nullable: true })
+  price?: number | null;
+
+  @Column({ nullable: true })
+  height?: number | null;
+
+  @Column({ nullable: true })
+  width?: number | null;
+
+  @Column({ nullable: true })
+  depth?: number | null;
 
   @Column({ default: false })
   isActive: boolean;
+  @Column({ unique: true, nullable: true })
+  isbn?: string | null;
 
   @CreateDateColumn()
   createdAt: Date;
