3bbcca6750e9bea5acdacf9c1f4d1173c9cd6fe0
piotrproszowski <143588034+piotrproszowski@users.noreply.github.com>
2024-03-24 23:57:25 +0100

    refactor CreateBookDto to include additional properties

--- src/structure/books/dto/create-book.dto.ts
+++ src/structure/books/dto/create-book.dto.ts
@@ -1,31 +1,104 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsBoolean, IsISBN, IsOptional, IsString } from 'class-validator';
+import { Transform } from 'class-transformer';
+import {
+  IsBoolean,
+  IsDateString,
+  IsNumber,
+  IsOptional,
+  IsString,
+} from 'class-validator';
 
 export class CreateBookDto {
   @ApiProperty({ required: true })
   @IsString()
-  readonly title: string;
+  title: string;
+
+  @ApiProperty({ required: false, nullable: true })
+  @IsOptional()
+  @IsString()
+  description?: string;
+
+  @ApiProperty({ required: false, nullable: true })
+  @IsOptional()
+  @Transform(({ value }) => Number(value) || null)
+  @IsNumber()
+  price?: number | null;
+
+  @ApiProperty({ required: false, nullable: true })
+  @IsOptional()
+  @IsString()
+  publicationLanguage?: string | null;
+
+  @ApiProperty({ required: false, nullable: true })
+  @IsOptional()
+  @IsString()
+  originalLanguage?: string | null;
+
+  @ApiProperty({ required: false, nullable: true })
+  @IsOptional()
+  @Transform(({ value }) => parseInt(value) || null)
+  @IsNumber()
+  numberOfPages?: number | null;
+
+  @ApiProperty({ required: false, nullable: true })
+  @IsOptional()
+  @Transform(({ value }) => parseInt(value) || null)
+  @IsNumber()
+  editionNumber?: number | null;
+
+  @ApiProperty({ required: false, nullable: true })
+  @IsOptional()
+  @IsDateString()
+  releaseDate?: Date | null;
+
+  @ApiProperty({ required: false, nullable: true })
+  @IsOptional()
+  @IsDateString()
+  publicationDate?: Date | null;
+
+  @ApiProperty({ required: false, nullable: true })
+  @IsOptional()
+  @IsString()
+  format?: string | null;
+
+  @ApiProperty({ required: false, nullable: true })
+  @IsOptional()
+  @IsString()
+  coverType?: string | null;
+
+  @ApiProperty({ required: false, nullable: true })
+  @IsOptional()
+  @IsString()
+  author?: string | null;
+
+  @ApiProperty({ required: false, nullable: true })
+  @IsOptional()
+  @IsString()
+  publisher?: string | null;
+
+  @ApiProperty({ required: false, nullable: true })
+  @IsOptional()
+  @Transform(({ value }) => parseInt(value) || null)
+  @IsNumber()
+  height?: number | null;
+
+  @ApiProperty({ required: false, nullable: true })
+  @Transform(({ value }) => parseInt(value) || null)
+  @IsNumber()
+  width?: number | null;
+
+  @ApiProperty({ required: false, nullable: true })
+  @IsOptional()
+  @Transform(({ value }) => parseInt(value) || null)
+  @IsNumber()
+  depth?: number | null;
 
   @ApiProperty({ required: true })
-  @IsString()
-  readonly author: string;
-
-  @ApiProperty({ required: false })
-  @IsOptional()
-  @IsString()
-  readonly description?: string;
-
-  @ApiProperty({ required: true })
-  @IsISBN()
-  readonly isbn: string;
-
-  @ApiProperty({ required: false })
-  @IsOptional()
-  @IsString()
-  readonly price?: string;
-
-  @ApiProperty({ required: false })
-  @IsOptional()
   @IsBoolean()
-  readonly isActive: boolean;
+  isActive: boolean;
+
+  @ApiProperty({ required: false, nullable: true })
+  @IsOptional()
+  @IsString()
+  isbn?: string | null;
 }
