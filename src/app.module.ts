cd3cce0ebc5e1cb50a0be941a2788064df4192fa
piotrproszowski <143588034+piotrproszowski@users.noreply.github.com>
2025-03-11 00:32:43 +0100

    refactor(app): restructure module imports and add Swagger documentation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,29 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
-import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
-
-import { StructureModule } from './structure/structure.module';
-import { DatabaseModule } from './database/database.module';
-import { ConfigModule } from './config/config.module';
-import { MailerModule } from './mailer/mailer.module';
 import { APP_GUARD } from '@nestjs/core';
-import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
-import { RolesGuard } from './auth/guards/roles.guard';
-import { RentModule } from './rent/rent.module';
+
+import { SharedModule } from './domains/shared/shared.module';
+import { UserModule } from './domains/user/user.module';
+import { AuthModule } from './domains/auth/auth.module';
+import { LibraryModule } from './domains/library/library.module';
+import { RentalModule } from './domains/rental/rental.module';
+import { JwtAuthGuard } from './domains/auth/infrastructure/guards/jwt-auth.guard';
+import { RolesGuard } from './domains/auth/infrastructure/guards/roles.guard';
 
 @Module({
-  imports: [
-    ConfigModule,
-    DatabaseModule,
-    MailerModule,
-    AuthModule,
-    UsersModule,
-    StructureModule,
-    RentModule,
-  ],
+  imports: [SharedModule, UserModule, AuthModule, LibraryModule, RentalModule],
   controllers: [AppController],
   providers: [
     {
--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import * as bodyParser from 'body-parser';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    abortOnError: false,
-    bufferLogs: true,
-  });
+  const app = await NestFactory.create(AppModule);
 
-  app.use(bodyParser.json({ limit: '50mb' }));
-  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+  app.useGlobalPipes(new ValidationPipe());
 
-  // app.set('trust proxy', 1);
-  app.enableCors();
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  const config = new DocumentBuilder()
+    .setTitle('Library API')
+    .setDescription('The Library API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
 }
+
 bootstrap();
