18a4502b88fa7e699ce161bb83d0e272c058b52c
piotrproszowski <143588034+piotrproszowski@users.noreply.github.com>
2025-03-15 18:29:55 +0100

    refactor(mailer): simplify mailer module configuration

--- src/mailer/mailer.module.ts
+++ src/mailer/mailer.module.ts
@@ -9,39 +9,36 @@ import { join } from 'node:path';
     NestMailerModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        preview:
-          process.env.NODE_ENV === 'development' ? { open: true } : false,
-        transport: {
-          host: configService.get('mail.host'),
-          port: configService.get('mail.port'),
-          ignoreTLS: true,
-          secure: configService.get('mail.secure'),
-          auth: {
-            user: configService.get('mail.user'),
-            pass: configService.get('mail.pass'),
+      useFactory: (configService: ConfigService) => {
+        const templatesDir = join(__dirname, 'templates');
+
+        return {
+          preview:
+            process.env.NODE_ENV === 'development' ? { open: true } : false,
+          transport: {
+            host: configService.get('mail.host'),
+            port: configService.get('mail.port'),
+            ignoreTLS: true,
+            secure: configService.get('mail.secure'),
+            auth: {
+              user: configService.get('mail.user'),
+              pass: configService.get('mail.pass'),
+            },
           },
-        },
-        defaults: {
-          from: configService.get('mail.from'),
-          template: 'index',
-        },
-        template: {
-          dir: join(__dirname, 'templates'),
-          adapter: new HandlebarsAdapter(undefined, {
-            inlineCssEnabled: true,
-            //@ts-expect-error kkk
-            inlineCssOptions: { url: ' ', preserveMediaQueries: true },
-          }),
-          options: { strict: false },
-        },
-        options: {
-          partials: {
-            dir: join(__dirname, 'templates/partials'),
-            options: { strict: false },
+          defaults: {
+            from: configService.get('mail.from'),
           },
-        },
-      }),
+          template: {
+            dir: templatesDir,
+            adapter: new HandlebarsAdapter(undefined, {
+              inlineCssEnabled: true,
+            }),
+            options: {
+              strict: false,
+            },
+          },
+        };
+      },
     }),
   ],
 })
